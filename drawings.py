21cac6c36d9ad36d6bb987a26ccc7c38495674f1
Mike-Jorgensen <JorgensenMB@gmail.com>
2022-12-05 12:22:52 -0600

    Complete meme generator, needs ttf or otf file for typeface

--- drawings.py
+++ drawings.py
@@ -1,5 +1,9 @@
 
 import PIL.Image
+import os
+import PIL.ImageFont
+import PIL.ImageDraw
+import textwrap
 
 def draw_border(image, border_size, border_color):
         original_width, original_height = image.size
@@ -8,3 +12,58 @@ def draw_border(image, border_size, border_color):
         border_canvas = PIL.Image.new('RGB', (width, height), border_color)
         border_canvas.paste(image, (border_size, border_size))
         return border_canvas
+
+def draw_text(image, text, text_color, text_size):
+        font = get_font(size=text_size)
+        draw_canvas = PIL.ImageDraw.Draw(image)
+        text_width, text_height = draw_canvas.textsize(text, font=font)
+        image_width = image.size[0]
+        if text_width > image_width:
+                character_width = text_width / len(text)
+                max_character_count = int( image_width / character_width)
+                text_lines = wrap_text(text, wrap_width=max_character_count)
+        else:
+                text_lines = [text]
+
+        for row, line in enumerate(text_lines):
+                draw_canvas.text((0, row * text_height), line, fill=text_color, font=font)
+        return image
+
+def get_text_as_image(text, text_color, text_size, image_width, background_color):
+        placeholder = PIL.Image.new('RGB', (0,0), background_color)
+        font = get_font(size=text_size)
+        draw_canvas = PIL.ImageDraw.Draw(placeholder)
+        text_width, text_height = draw_canvas.textsize(text, font=font)
+        if text_width > image_width:
+                character_width = text_width / len(text)
+                max_characters_count = int(image_width / character_width)
+                text_lines = wrap_text(text, wrap_width=max_characters_count)
+        else:
+                text_lines = [text]
+
+        total_text_height = len(text_lines) * text_height
+        image = PIL.Image.new('RGB', (image_width, total_text_height), background_color)
+        draw_canvas = PIL.ImageDraw.Draw(image)
+
+        for row, line in enumerate(text_lines):
+                row_height = row * text_height
+                line_width, _ = draw_canvas.textsize(line, font=font)
+                left = (image_width - line_width) / 2
+                draw_canvas.text((left, row_height), line, fill=text_color, font=font)
+        return image
+
+def bottom_expand_image_with_image(image, expand_image, background_color):
+        width = image.size[0]
+        height = image.size[1] + expand_image.size[1]
+        expand_canvas = PIL.Image.new('RGB', (width, height), background_color)
+        expand_canvas.paste(image, (0,0))
+        expand_canvas.paste(expand_image, (0, image.size[1]))
+        return expand_canvas
+
+def wrap_text(text, wrap_width):
+        wrapper = textwrap.TextWrapper(width=wrap_width)
+        return wrapper.wrap(text)
+
+def get_font(size):
+        path = os.path.join('./Roboto-Regular.ttf') #set this path to font file
+        return PIL.ImageFont.truetype(path, size=size)
--- meme_generator.py
+++ meme_generator.py
@@ -2,13 +2,24 @@ import PIL.Image
 import drawings
 
 class MemeGenerator:
+    TEXT_COLOR = 'white'
+    BACKGROUND_COLOR = 'black'
+
     def __init__(self, image_path, caption):
         self.image_path = image_path
         self.caption = caption
 
     def generate(self):
         image = self.get_image_object()
-        image = drawings.draw_border(image, border_size=10, border_color='black')
+        text_image = drawings.get_text_as_image(
+            self.caption,
+            text_color = self.TEXT_COLOR,
+            text_size=30,
+            image_width=image.size[0],
+            background_color=self.BACKGROUND_COLOR
+        )
+        image = drawings.bottom_expand_image_with_image(image, text_image, background_color=self.BACKGROUND_COLOR)
+        image = drawings.draw_border(image, border_size=10, border_color=self.BACKGROUND_COLOR)
         return image
 
     def get_image_object(self):
